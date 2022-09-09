9c702ab0cee640d47a607855221348755357d9ab
daniaraujoxx <danyeladearaujo@hotmail.com>
2022-09-09 14:15:23 -0300

    Inclusão de login'

--- src/main/java/com/example/myapplication/MainActivity.kt
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,41 +1,26 @@
 package com.example.myapplication
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSalvar: Button = findViewById(R.id.btnSalvar)
-        val edt_nome: TextView = findViewById(R.id.edt_nome)
-        val edt_email: TextView = findViewById(R.id.edt_email)
-        val edt_senha: TextView = findViewById(R.id.edt_senha)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        val btnCriarConta = findViewById<Button>(R.id.btn_criarConta)
+
+        btnCriarConta.setOnClickListener{
+            val i = Intent(this, CadastroActivity::class.java)
+            startActivity(i)
+        }
 
 
-        btnSalvar.setOnClickListener(View.OnClickListener {
 
-            val cadastroPersistencia = this.getSharedPreferences("cadastro", Context.MODE_PRIVATE)
-            val editor = cadastroPersistencia.edit()
-
-            editor.putString("nome",edt_nome.text.toString())
-            editor.putString("email",edt_email.text.toString())
-            editor.putString("senha",edt_senha.text.toString())
-            editor.apply()
-
-
-            Toast.makeText(this,"Cadastro realizado!",Toast.LENGTH_SHORT).show()
-
-
-        })
 
     }
 }
